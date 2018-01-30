bf0e44c4c1f4b2ff1a5c7a62e938857bafc8f3b1
Jérôme Skoda <contact@jeromeskoda.fr>
2018-01-30 01:31:34 +0100

    Setup build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require("gulp");
 var pug = require('gulp-pug');
 var ts = require("gulp-typescript");
+var stylus = require('gulp-stylus');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
 
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('default', ['ts', 'public', 'views']);
+gulp.task('default', ['ts', 'views', 'public_stylesheets', 'public_javascripts', 'public_image']);
 
 gulp.task('ts', function () {
   return tsProject.src()
@@ -12,12 +19,42 @@ gulp.task('ts', function () {
     .js.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('public', function () {
-  return gulp.src('./src/public/**/*')
-    .pipe(gulp.dest('./dist/public/'));
-});
-
 gulp.task('views', function () {
   return gulp.src('./src/views/**/*')
     .pipe(gulp.dest('./dist/views/'));
+});
+
+gulp.task('public_javascripts', function(){
+   gulp.src('src/public/javascripts/**/*.js')
+   .pipe(concat('script.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('dist/public/javascripts'));
+});
+
+gulp.task('public_image', function() {
+   gulp.src('src/public/images/**/*.+(png|jpg|gif)')
+   .pipe(changed('dist/public/images/'))
+   .pipe(imagemin([
+     imagemin.gifsicle({interlaced: true}),
+imagemin.jpegtran({progressive: true}),
+imagemin.optipng({optimizationLevel: 5}),
+   ]))
+   .pipe(gulp.dest('dist/public/images/'));
+});
+
+
+gulp.task('public_stylesheets', function () {
+
+  var filter_stylus = filter('**/*.styl', { restore: true });
+
+  return gulp.src([
+    'src/public/stylesheets/**/*.styl',
+    'src/public/stylesheets/**/*.css'
+  ])
+  .pipe(filter_stylus)
+  .pipe(stylus())
+  .pipe(filter_stylus.restore)
+  .pipe(concat('out.css'))
+  .pipe(gulp.dest('dist/public/stylesheets'));
+
 });
